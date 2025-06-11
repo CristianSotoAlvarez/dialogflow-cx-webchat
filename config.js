ce5f98e191c97ab7af7b5e956fcb0dba60de67ea
Alan <alan.baeza.o@mail.pucv.cl>
2025-06-11 16:39:39 -0400

    hotfix

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-/*COPIAR LA DIRECCION DEL ENTORNO AQUI, AGREGAR "/chat" AL FINAL Y GUARDAR COMO 
-VARIABLE "BACKEND_URL" SI ES LA QUE SE VA A UTILIZAR!!!*/
+/*COPIAR LA DIRECCION DEL ENTORNO AQUI, AGREGAR "/chat" AL FINAL
+GUARDAR COMO VARIABLE "BACKEND_URL" SI ES LA QUE SE VA A UTILIZAR!!!*/
 
 const CONFIG = {
     BACKEND_URLA: 'https://e2da987d-b77a-4872-a909-5187ce34a4ae-00-psjl3t6ye9l3.riker.replit.dev/chat', //Backend orignial (Crisitan)
-    BACKEND_URLB: 'https://1710c062-6d15-48bd-8c91-d84e58d2c801-00-3pulep6quvnbk.spock.replit.dev/chat', //Backend copia (Alan)
+    BACKEND_URL: 'https://1710c062-6d15-48bd-8c91-d84e58d2c801-00-3pulep6quvnbk.spock.replit.dev/chat', //Backend copia (Alan)
 };
 
 export default CONFIG;
